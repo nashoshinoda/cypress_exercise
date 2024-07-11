d505a56a35c4023d6f3e8a5605788d5e407cf2a2
Ignacio Velderrain <Ignacio.Velderrain@commscope.com>
2024-07-11 10:33:34 -0700

    Add Allure Report into the project.

--- cypress.config.js
+++ cypress.config.js
@@ -1,11 +1,13 @@
 const { defineConfig } = require("Cypress");
 const cucumber = require("cypress-cucumber-preprocessor").default;
+const { allureCypress } = require("allure-cypress/reporter");
 
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
       on("file:preprocessor", cucumber());
+      allureCypress(on);
     },
     specPattern: "cypress/e2e/*.feature",
   },
